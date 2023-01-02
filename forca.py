23ae9620cef6c0867085a84eac3c99307c75d266
Victor Hugo Santos <victorhugossantos@hotmail.com>
2023-01-02 13:49:15 -0300

    adicao do forca.py

--- forca.py
+++ forca.py
@@ -3,7 +3,28 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Digite uma letra: ")
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+
+
+        print("Jogando...")
+
+
     print("Fim do jogo")
+
 
 if(__name__ == "__main__"):
     jogar()
